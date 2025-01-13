104d7266710225d22c11bdf3309e65ef8c83c367
Twullur <189119157+Twullur@users.noreply.github.com>
2025-01-13 17:57:56 +0700

    complete the exercises 1 to 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string,sum) {
+    if (sum >= 0) {
+      let adjustedString = "";
+      for (let i = 0; i < sum; i++) { 
+        adjustedString += string;
+      }
+      return adjustedString;
+    } else return "ERROR";
+  }
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let reversedString = "";
+    for (i = string.length; i >= 0; i--) {
+        reversedString += string.slice(i-1,i);
+    }
+    return reversedString;
+}
+  
 
 // Do not edit below this line
 module.exports = reverseString;
